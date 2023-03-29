29dac6b85c657499f0e7d19ce5892cc591da2544
Sellucas <lucassellmachado@gmail.com>
2023-03-29 15:43:00 -0300

    feature: collect name email image from user session

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,19 +1,26 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import { Avatar } from "@mui/material";
 
 const Login = () => {
   const { data: session } = useSession();
+  const userProfileImg = session?.user?.image as string;
 
   if (session) {
-    return <>
+    return (
+      <>
         Signed in as {session?.user?.email} <br />
+        <p>Welcome {session?.user?.name}</p>
+        <Avatar alt={session?.user?.name as string} src={userProfileImg} />
         <button onClick={() => signOut()}>Sign Out</button>
       </>
-    }
-    return <>
-        Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
-      </>
-  
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
 };
 
 export default Login;
