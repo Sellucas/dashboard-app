e611c81be46b604011c78e892eda638f88ff078a
Sellucas <lucassellmachado@gmail.com>
2023-03-31 14:14:36 -0300

    feat: toggling color mode setup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -62,7 +62,7 @@ const Header = () => {
               textDecoration: "none",
             }}
           >
-            LOGO
+            QuickView
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
@@ -118,7 +118,7 @@ const Header = () => {
               textDecoration: "none",
             }}
           >
-            LOGO
+            QuickView
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,74 @@
-import "@/styles/globals.css";
+import React from "react";
+import {
+  Box,
+  createTheme,
+  CssBaseline,
+  IconButton,
+  ThemeProvider,
+  useTheme,
+} from "@mui/material";
 import { SessionProvider } from "next-auth/react";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+const App = ({ Component, pageProps: { session, ...pageProps } }) => {
+  const [mode, setMode] = React.useState<"light" | "dark">("dark");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
   );
-}
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <SessionProvider session={session}>
+          <Box
+            sx={{
+              display: "flex",
+              width: "100%",
+              alignItems: "center",
+              justifyContent: "center",
+              bgcolor: "background.default",
+              color: "text.primary",
+              borderRadius: 1,
+              p: 3,
+            }}
+          >
+            {theme.palette.mode} mode
+            <IconButton
+              sx={{ ml: 1 }}
+              onClick={colorMode.toggleColorMode}
+              color="inherit"
+            >
+              {theme.palette.mode === "dark" ? (
+                <Brightness7Icon />
+              ) : (
+                <Brightness4Icon />
+              )}
+            </IconButton>
+          </Box>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
+
+export default App;
