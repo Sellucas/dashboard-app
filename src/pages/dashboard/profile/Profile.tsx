e13367c93373c61af14107f04bd015c54dde2572
Sellucas <lucassellmachado@gmail.com>
2023-05-23 14:32:45 -0300

    feat: profile form

--- src/pages/dashboard/profile/Profile.tsx
+++ src/pages/dashboard/profile/Profile.tsx
@@ -1,9 +1,143 @@
-import React from 'react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useSession } from "next-auth/react";
+import React from "react";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { data: session } = useSession();
+  const handleSubmit = () => {};
 
-export default Profile
+  const formData = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    receiveEmails: false,
+  };
+
+  const handleFormChange = () => {};
+
+  return (
+    <>
+      <h1>Profile</h1>
+      <Box>
+        <Typography variant={"h4"} sx={{ paddingBottom: 4 }}>
+          Hey {session ? session?.user?.name : "User"}, welcome to your profile
+          👋
+        </Typography>
+        <Paper sx={{ padding: "1rem 2rem" }}>
+          <Grid container justifyContent="center">
+            <Grid item xs={12} sm={8} md={6}>
+              <Box display="flex" flexDirection="column" alignItems="center">
+                <Avatar
+                  sx={{
+                    height: 100,
+                    width: 100,
+                    marginBottom: 2,
+                  }}
+                  src={session?.user?.image as string}
+                />
+              </Box>
+              <form
+                onSubmit={handleSubmit}
+                style={{ maxWidth: 600, margin: "0 auto" }}
+              >
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      label="First Name"
+                      name="firstName"
+                      value={formData.firstName}
+                      onChange={handleFormChange}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      label="Last Name"
+                      name="lastName"
+                      value={formData.lastName}
+                      onChange={handleFormChange}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      type="email"
+                      label="Email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleFormChange}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      type="password"
+                      label="Password"
+                      name="password"
+                      value={formData.password}
+                      onChange={handleFormChange}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      type="password"
+                      label="Confirm Password"
+                      name="confirmPassword"
+                      value={formData.confirmPassword}
+                      onChange={handleFormChange}
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          name="receiveEmails"
+                          checked={formData.receiveEmails}
+                          onChange={handleFormChange}
+                          color="primary"
+                        />
+                      }
+                      label="Receive sales analytics emails"
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Button type="submit" variant="contained" color="primary">
+                      Save Changes
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
+    </>
+  );
+};
+
+export default Profile;
