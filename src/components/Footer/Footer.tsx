be0ea45f2180edbbbca021c7e5e1d1b982f6d47e
Sellucas <lucassellmachado@gmail.com>
2023-06-11 15:38:14 -0300

    feat: footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,49 +1,108 @@
-import React from "react";
-import scss from "./Footer.module.scss";
-import { Paper, useTheme } from "@mui/material";
-import styled from "@emotion/styled";
-import Link from "next/link";
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { Box } from "@mui/material";
+import scss from './Footer.module.scss'
+import StackedBarChartSharpIcon from "@mui/icons-material/StackedBarChartSharp";
 
-const Footer = () => {
-  const theme = useTheme();
-
-  const FooterLink = styled(Link)`
-    color: ${theme.palette.text.primary};
-  `;
-
+export default function Footer() {
   return (
-    <footer className={scss.footer}>
-      <Paper
-        sx={{ width: "100%", backgroundColor: "#161d20" }}
-        variant="elevation"
-      >
-        <ul role="menu">
-          <li>
-            <FooterLink href={"/"}>Home</FooterLink>
-          </li>
-          <li>
-            <FooterLink href={"/dashboard/data"}>Data</FooterLink>
-          </li>
-          <li>
-            <FooterLink href={"/dashboard/profile"}>Profile</FooterLink>
-          </li>
-          <li>
-            <FooterLink href={"/dashboard/settings"}>Settings</FooterLink>
-          </li>
-          <li>
-            <FooterLink href={"/#termsandconditions"}>
-              Terms & Conditions
-            </FooterLink>
-          </li>
-          <li>
-            <FooterLink href={"/#accessibilitystatement"}>
-              Accessibility statement
-            </FooterLink>
-          </li>
-        </ul>
-      </Paper>
-    </footer>
+    <Box
+      component="footer"
+      sx={{
+        background: "#2d3336",
+        marginTop: "6rem",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <div className={scss.logo}>
+              <StackedBarChartSharpIcon sx={{ mr: 1 }} fontSize="medium" />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  fontWeight: 500,
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+                className={scss.logoName}
+              >
+                QuickView
+              </Typography>
+            </div>
+            <Typography variant="body2" color="text.secondary">
+              Your intuitive and customizable dashboard platform. Access
+              real-time insights, share interactive panels, and make strategic
+              decisions with ease. Take your data analysis to the next level
+              with QuickView.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Contact
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              123 Main Street, Anytown, USA
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Email: info@example.com
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Phone: +1 234 567 8901
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link
+              href="https://www.facebook.com/"
+              color="inherit"
+              target="_blank"
+            >
+              <Facebook />
+            </Link>
+            <Link
+              href="https://www.instagram.com/"
+              color="inherit"
+              sx={{ pl: 1, pr: 1 }}
+              target="_blank"
+            >
+              <Instagram />
+            </Link>
+            <Link
+              href="https://www.twitter.com/"
+              color="inherit"
+              target="_blank"
+            >
+              <Twitter />
+            </Link>
+          </Grid>
+        </Grid>
+        <Box mt={5}>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {"Copyright © "}
+            <Link
+              color="inherit"
+              href="https://github.com/Sellucas"
+              target="_blank"
+            >
+              Sellucas
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
   );
-};
-
-export default Footer;
+}
