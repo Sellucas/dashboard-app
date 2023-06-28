76626d8d4ed7a11416e7908b4f2a61d03405b1c4
Sellucas <lucassellmachado@gmail.com>
2023-06-28 13:39:35 -0300

    update: signin auth

--- src/pages/auth/signIn/SignIn.tsx
+++ src/pages/auth/signIn/SignIn.tsx
@@ -1,11 +1,8 @@
 import Login from "@/components/Login/Login";
 import { Box } from "@mui/material";
-import { useSession } from "next-auth/react";
 import React from "react";
 
 const SignIn = () => {
-  const { data: session } = useSession();
-
   return (
     <Box
       sx={{
@@ -13,9 +10,9 @@ const SignIn = () => {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        marginTop: "2rem",
       }}
     >
-      <h2>{session ? "Thank you for logging in" : "Please log in"}</h2>
       <Login />
     </Box>
   );
